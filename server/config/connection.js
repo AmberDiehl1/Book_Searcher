656b96f0398546571c2e0dd258480368df207a21
AmberDiehl1 <amberdiehl@outlook.com>
2023-11-30 21:08:17 -0500

    heroku take two

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://amberdiehl:WebDev.01@cluster0.u2ugabu.mongodb.net/Cluster0?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
